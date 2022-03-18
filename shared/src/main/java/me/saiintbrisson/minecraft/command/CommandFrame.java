5dde6dba4873bb9d97bba260814c55916bdf2b62
saiintbrisson <luizcmpc@gmail.com>
2022-03-17 22:17:30 -0300

    chore(shared): reorgnized and completed docs for `CommandFrame`

--- shared/src/main/java/me/saiintbrisson/minecraft/command/CommandFrame.java
+++ shared/src/main/java/me/saiintbrisson/minecraft/command/CommandFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Luiz Carlos Mourão Paes de Carvalho
+ * Copyright 2020 Luiz Carlos Carvalho Paes de Carvalho
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -18,66 +18,80 @@ package me.saiintbrisson.minecraft.command;
 
 import me.saiintbrisson.minecraft.command.argument.AdapterMap;
 import me.saiintbrisson.minecraft.command.argument.TypeAdapter;
-import me.saiintbrisson.minecraft.command.argument.eval.MethodEvaluator;
-import me.saiintbrisson.minecraft.command.command.CommandHolder;
 import me.saiintbrisson.minecraft.command.command.CommandInfo;
 import me.saiintbrisson.minecraft.command.executor.CommandExecutor;
 import me.saiintbrisson.minecraft.command.executor.CompleterExecutor;
-import me.saiintbrisson.minecraft.command.message.MessageHolder;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Map;
-import java.util.concurrent.Executor;
 
 /**
  * The CommandFrame is the core of the framework,
- * it registers the commands, adapters {@link AdapterMap}
- * and message holders {@link MessageHolder}
+ * it registers the commands, adapters {@link AdapterMap}.
  *
- * @author Luiz Carlos Mourão
+ * @author Luiz Carlos Carvalho
  */
-public interface CommandFrame<P, S, C extends CommandHolder<S, ? extends C>> {
-    P getPlugin();
-    AdapterMap getAdapterMap();
-    MessageHolder getMessageHolder();
-
-    Map<String, C> getCommandMap();
-    MethodEvaluator getMethodEvaluator();
-
-    @Nullable
-    Executor getExecutor();
-
-    C getCommand(String name);
+public interface CommandFrame<P, S> {
+    /**
+     * Get the plugin that owns this frame instance.
+     *
+     * @return the plugin that owns this frame instance.
+     */
+    P plugin();
 
     /**
-     * Registers a new Adapter from that type.
-     * @param type Class
-     * @param adapter TypeAdapter
-     * @param <T> Generic value for the type
+     * Get the adapter map used by this frame instance
+     * and its commands.
+     *
+     * @return the adapter map.
+     */
+    AdapterMap getAdapterMap();
+
+    /**
+     * Registers a new adapter.
+     *
+     * <pre>{@code
+     * frame.registerAdapter(Integer.class, (string) -> {
+     *     return Integer.valueOf(string) * 10;
+     * });
+     * }</pre>
+     *
+     * @param type    the target class.
+     * @param adapter the mapping function.
+     * @param <T>     the target type.
      */
     default <T> void registerAdapter(Class<T> type, TypeAdapter<T> adapter) {
         getAdapterMap().put(type, adapter);
     }
 
     /**
-     * Registers all methods annotated with {@link me.saiintbrisson.minecraft.command.annotation.Command}.
-     * @param objects Object...
+     * Registers all public methods annotated with
+     * {@link me.saiintbrisson.minecraft.command.annotation.Command Command},
+     * {@link me.saiintbrisson.minecraft.command.annotation.Completer Completer},
+     * and {@link me.saiintbrisson.minecraft.command.annotation.ExceptionHandler ExceptionHandler}.
+     *
+     * @param classes the classes to be searched for.
      */
-    void registerCommands(Object... objects);
+    void registerAll(Object... classes);
 
     /**
      * Registers a single command with the CommandInfo and Executor
-     * @param commandInfo CommandInfo
-     * @param commandExecutor CommandExecutor
+     *
+     * @param commandInfo the command metadata.
+     * @param executor    the command to be executed.
      */
-    void registerCommand(CommandInfo commandInfo, CommandExecutor<S> commandExecutor);
-    void registerCompleter(String name, CompleterExecutor<S> completerExecutor);
+    void registerCommand(CommandInfo commandInfo, CommandExecutor<S> executor);
+
+    /**
+     * Registers a single completer for the given command.
+     *
+     * @param name     the command name.
+     * @param executor the completer to be executed.
+     */
+    void registerCompleter(String name, CompleterExecutor<S> executor);
 
     /**
      * Unregisters a command.
-     * @param name the command to delete
      *
-     * @return whether the command existed or not
+     * @param name the command to delete.
+     * @return whether the command existed or not.
      */
     boolean unregisterCommand(String name);
 }
